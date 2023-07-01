abaeb759aeee328f3dc2c6b2e13dcfaaebc86647
GetialJuan <getial.juan@correounivalle.edu.co>
2023-07-01 17:06:00 -0500

    Add solution method - In this method will be the fina solution - Now, it orders the parts and get the max/min participation of animals

--- solucion.py
+++ solucion.py
@@ -25,56 +25,6 @@ partes = [parte_1, parte_2]
 
 
 ### Funciones de ordenamiento para listas de animales
-
-## Retorna la grandeza total de una escena (de animales)
-## y la  grandeza maxima,
-# Complexity O(n)
-def grandeza_total_y_max(escena, grandezas):
-    animal0 = escena[0]
-    grandeza_total = grandezas[animal0]
-    grandeza_max = grandezas[animal0]
-    for i in range(1, len(escena)):
-        animal1 = escena[i]
-        grandeza_total += grandezas[animal1]
-        if grandezas[animal1] > grandezas[animal0]:
-            grandeza_max = grandezas[animal1]
-            animal0 = animal1
-    return (grandeza_total, grandeza_max)
-
-#Ordena una escena
-def counting_sort(scene, animals):
-    size = len(scene)
-    output = [0] * size
-    (grandeza_total, grandeza_max) = grandeza_total_y_max(scene, animals)
-    # Initialize count array
-    max_index = grandeza_max+1
-    count = [0] * max_index
-
-    # Store the count of each elements in count array
-    for i in range(0, size):
-        count[animals[scene[i]]] += 1
-
-    # Store the cummulative count
-    for i in range(1, max_index):
-        count[i] += count[i - 1]
-    # Sorting
-    i = size - 1
-
-    animals_list = list(animals.keys())
-    grandezas_list = list(animals.values())
-
-    while i >= 0:
-        grandeza = animals[scene[i]]
-        index = grandezas_list.index(grandeza)
-        output[count[grandeza] - 1] = animals_list[index]
-        count[grandeza] -= 1
-        i -= 1
-
-    return [output, grandeza_total, grandeza_max]
-
-
-
-
 def find_max_value(lst):
     if not lst:
         return None
@@ -86,6 +36,18 @@ def find_max_value(lst):
             max_value = num
     
     return max_value
+
+def find_min_value(lst):
+    if not lst:
+        return None
+    
+    min_value = lst[0]
+    
+    for num in lst:
+        if num < min_value:
+            min_value = num
+    
+    return min_value
 
 ## Ordena las escenas segun su grandeza max
 def counting_sort_scenes_grandeza_max(array, grandezas_max):
@@ -150,31 +112,12 @@ def counting_sort_scenes(array, grandezas_totales):
     for i in range(0, size):
         array[i] = output[i]
 
-# ordena una parte.
-# una parte es una lista de escenas
-def ordenar_parte(parte):
-    print("****************************Ordenando Todas las Escenas*************")
-    escenas_Ordenadas_Internas = []
-    grandezas_totales = []
-    grandezas_max = []
-    grandeza_total_escena = 0
-    for escena in parte:
-        (escena_ordenada, grandeza_total, grandeza_max) = counting_sort(escena, ANIMALES)
-        escenas_Ordenadas_Internas.append((escena_ordenada, grandeza_total, grandeza_max))
-        grandezas_totales.append(grandeza_total)
-        grandezas_max.append(grandeza_max)
-        grandeza_total_escena += grandeza_total
-
-    counting_sort_scenes_grandeza_max(escenas_Ordenadas_Internas, grandezas_max)
-    counting_sort_scenes(escenas_Ordenadas_Internas, grandezas_totales)
-    return [escenas_Ordenadas_Internas, grandeza_total_escena]
-
 def counting_sort_parts(parts, grandezas_totales):
     size = len(parts) # Numero de Partes
     output = [0] * size
     max_index=find_max_value(grandezas_totales) + 1
     # Initialize count array
-    count = [0] * max_index  #URGENTE CAMBIAR ESTO
+    count = [0] * max_index 
 
     # Store the count of each elements in count array
     for i in range(0, size):
@@ -199,15 +142,109 @@ def counting_sort_parts(parts, grandezas_totales):
     for i in range(0, size):
         parts[i] = output[i]
 
-def ordenar_partes(partes):
-    partes_ordenadas = []
-    grandezas_totales = []
-    for parte in partes:
-        parte_ordenada = ordenar_parte(parte)
-        partes_ordenadas.append(parte_ordenada)
-        grandezas_totales.append(parte_ordenada[1])
+def solution(partes, grandezas):
+    animales = grandezas.keys()
+    count_animals = {}
+    for animal in animales:
+        count_animals[animal] = 0
 
-    counting_sort_parts(partes_ordenadas, grandezas_totales)
-    print(partes_ordenadas)
+    ## Retorna la grandeza total de una escena (de animales)
+    ## y la  grandeza maxima, ademas de ir contando las apariciones
+    ## de cada animal
+    # Complexity O(n)
+    def grandeza_total_y_max(escena, grandezas):
+        animal0 = escena[0]
+        count_animals[animal0] += 1
+        grandeza_total = grandezas[animal0]
+        grandeza_max = grandezas[animal0]
+        for i in range(1, len(escena)):
+            animal1 = escena[i]
+            count_animals[animal1] += 1
+            grandeza_total += grandezas[animal1]
+            if grandezas[animal1] > grandezas[animal0]:
+                grandeza_max = grandezas[animal1]
+                animal0 = animal1
+        return (grandeza_total, grandeza_max)
+    
+    #Ordena una escena
+    def counting_sort(scene, animals):
+        size = len(scene)
+        output = [0] * size
+        (grandeza_total, grandeza_max) = grandeza_total_y_max(scene, animals)
+        # Initialize count array
+        max_index = grandeza_max+1
+        count = [0] * max_index
 
-ordenar_partes(partes)
+        # Store the count of each elements in count array
+        for i in range(0, size):
+            count[animals[scene[i]]] += 1
+
+        # Store the cummulative count
+        for i in range(1, max_index):
+            count[i] += count[i - 1]
+        # Sorting
+        i = size - 1
+
+        animals_list = list(animals.keys())
+        grandezas_list = list(animals.values())
+
+        while i >= 0:
+            grandeza = animals[scene[i]]
+            index = grandezas_list.index(grandeza)
+            output[count[grandeza] - 1] = animals_list[index]
+            count[grandeza] -= 1
+            i -= 1
+
+        return [output, grandeza_total, grandeza_max]
+    
+    # ordena una parte.
+    # una parte es una lista de escenas
+    def ordenar_parte(parte):
+        escenas_Ordenadas_Internas = []
+        grandezas_totales = []
+        grandezas_max = []
+        grandeza_total_escena = 0
+        for escena in parte:
+            (escena_ordenada, grandeza_total, grandeza_max) = counting_sort(escena, ANIMALES)
+            escenas_Ordenadas_Internas.append((escena_ordenada, grandeza_total, grandeza_max))
+            grandezas_totales.append(grandeza_total)
+            grandezas_max.append(grandeza_max)
+            grandeza_total_escena += grandeza_total
+
+        counting_sort_scenes_grandeza_max(escenas_Ordenadas_Internas, grandezas_max)
+        counting_sort_scenes(escenas_Ordenadas_Internas, grandezas_totales)
+        return [escenas_Ordenadas_Internas, grandeza_total_escena]
+    
+    def ordenar_partes(partes):
+        partes_ordenadas = []
+        grandezas_totales = []
+        for parte in partes:
+            parte_ordenada = ordenar_parte(parte)
+            partes_ordenadas.append(parte_ordenada)
+            grandezas_totales.append(parte_ordenada[1])
+
+        counting_sort_parts(partes_ordenadas, grandezas_totales)
+        print(partes_ordenadas)
+
+    print('Partes ordenadas:')
+    ordenar_partes(partes)
+
+    count_animals_values = list(count_animals.values())
+    max_animal = find_max_value(count_animals_values)
+    min_animal = find_min_value(count_animals_values)
+
+    max_animals = []
+    min_animals = []
+    for animal in count_animals:
+        value = count_animals[animal]
+        if value == max_animal:
+            max_animals.append([animal, value])
+        if value == min_animal:
+            min_animals.append([animal, value])
+
+    print('Animales que mas/menos participaron:')
+    print(max_animals)
+    print(min_animals)
+
+
+solution(partes, ANIMALES)
